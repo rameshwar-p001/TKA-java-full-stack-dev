ef240b18067da5f025716e4e8910eccfb5b35070
Patil Rameshwar <rameshwarr.p001@gmail.com>
2026-06-18 19:30:27 +0530

    18/jun

---
+++ Day5/p5.java
@@ -0,0 +1,17 @@
+//// Assingment operetor 
+
+package Day5;
+
+public class p5 {
+    public static void main(String[] args) {
+
+        int a=10;  // = Assingment
+
+        System.out.println(a+=2); // add assingment
+        System.out.println(a-=2); // Minus assingment
+        System.out.println(a*=2); // Multi assingment
+        System.out.println(a/=2); // Div assingment
+        System.out.println(a%=2); // Mode  assingment
+
+    }
+}
---
+++ Day5/p6.java
@@ -0,0 +1,29 @@
+//// Bit wise operator 
+
+package Day5;
+
+// public class p6 {
+//     public static void main(String[] args) {
+         
+//         System.out.println(7 & 6);  // And
+//         System.out.println(56 | 12);    // or
+//         System.out.println(98 ^ 12);    //X-Or
+//         System.out.println(~10);        // +ve complement
+//         System.out.println(~-(10));     // -ve complement
+//     }
+// }
+
+
+
+
+//// Home work 
+public class p6 {
+    public static void main(String[] args) {
+        System.out.println(4 & 2);
+        System.out.println(42 | 112);
+        System.out.println(41 ^ 132);
+        System.out.println(234 | 245);
+        System.out.println(41 & 22);
+    }
+
+}

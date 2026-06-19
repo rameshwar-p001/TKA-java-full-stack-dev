f46c668cc7ebf3856304b7784b57b72413dc03b1
Patil Rameshwar <rameshwarr.p001@gmail.com>
2026-06-20 00:50:05 +0530

    19/jun

--- Day5/p6.java
+++ Day5/p6.java
@@ -2,28 +2,30 @@
 
 package Day5;
 
-// public class p6 {
-//     public static void main(String[] args) {
-         
-//         System.out.println(7 & 6);  // And
-//         System.out.println(56 | 12);    // or
-//         System.out.println(98 ^ 12);    //X-Or
-//         System.out.println(~10);        // +ve complement
-//         System.out.println(~-(10));     // -ve complement
-//     }
-// }
-
-
-
-
-//// Home work 
 public class p6 {
     public static void main(String[] args) {
-        System.out.println(4 & 2);
-        System.out.println(42 | 112);
-        System.out.println(41 ^ 132);
-        System.out.println(234 | 245);
-        System.out.println(41 & 22);
+         
+        System.out.println(7 & 6);  // And
+        System.out.println(56 | 12);    // or
+        System.out.println(98 ^ 12);    //X-Or
+        System.out.println(~10);        // +ve complement
+        System.out.println(~-(10));     // -ve complement
+        System.out.println(5<<3);       // Bit wise left shift
+        System.out.println(5>>3);       // Bit wise right shift
     }
-
 }
+
+
+
+
+// //// Home work 
+// public class p6 {
+//     public static void main(String[] args) {
+//         System.out.println(4 & 2);
+//         System.out.println(42 | 112);
+//         System.out.println(41 ^ 132);
+//         System.out.println(234 | 245);
+//         System.out.println(41 & 22);
+//     }
+
+// }
---
+++ Day8/ddp/p1.java
@@ -0,0 +1,10 @@
+package Day8.ddp;
+
+public class p1 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+}

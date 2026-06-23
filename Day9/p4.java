507a759d2cfd8a164de2aaeb37ccda4c4485b263
Patil Rameshwar <rameshwarr.p001@gmail.com>
2026-06-23 22:48:58 +0530

    Practice que ans

--- Day9/p4.java
+++ Day9/p4.java
@@ -1,103 +1,85 @@
-// package Day9;
+package Day9;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// // //// Write a program that checks if a person’s age is greater than 18, then print "Adult".
-// // public class p4 {
+// //// Write a program that checks if a person’s age is greater than 18, then print "Adult".
+// public class p4 {
     
-// //     public static void main(String[] args) {
+//     public static void main(String[] args) {
 
 
         
-// //         Scanner a = new Scanner(System.in);
-// //         System.out.print("Enter a Your age : ");
-// //         int r=a.nextInt();
-// //         if (r>=18) {
-// //             System.out.println("Your Adult... ");
-// //         }else{
-// //             System.out.println("Your age is less than 18... ");
-// //         }
-// //     }
+//         Scanner a = new Scanner(System.in);
+//         System.out.print("Enter a Your age : ");
+//         int r=a.nextInt();
+//         if (r>=18) {
+//             System.out.println("Your Adult... ");
+//         }else{
+//             System.out.println("Your age is less than 18... ");
+//         }
+//     }
 
-// // }
+// }
 
 
-// // Write a program to check whether a number is divisible by 10.
+// Write a program to check whether a number is divisible by 10.
 
-// // public class p4 {
+// public class p4 {
 
-// //     public static void main(String[] args) {
-// //         Scanner s = new Scanner(System.in);
-// //         System.out.print("Enter Number...");
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter Number...");
         
-// //         int a=s.nextInt();
-// //         if (a % 10==0) {
-// //             System.out.println("yes it can divide by 10.. ");
-// //         }
-// //         else{
-// //             System.out.println("It can not be divisible ");
-// //         }
-// //     }
+//         int a=s.nextInt();
+//         if (a % 10==0) {
+//             System.out.println("yes it can divide by 10.. ");
+//         }
+//         else{
+//             System.out.println("It can not be divisible ");
+//         }
+//     }
 
-// // }
-
-
-// // Write a program to check whether a temperature is above 30°C, then print "Hot Day".
-
-// // public class p4 {
-
-// //     public static void main(String[] args) {
-// //         Scanner s =new Scanner(System.in);
-// //         System.out.println("Enter temperature");
-// //         int a=s.nextInt();
-
-// //         if (a>=30) {
-// //             System.out.println("Hot Day");
-// //         }
-// //         else{
-// //             System.out.println("normal temperature");
-// //         }
-// //     }
-// // }
+// }
 
 
-// // Write a program to check whether a given year is leap year or not.
+// Write a program to check whether a temperature is above 30°C, then print "Hot Day".
 
-// // public class p4 {
+// public class p4 {
 
-// //     public static void main(String[] args) {
-// //         Scanner s =new Scanner(System.in);
-// //         System.out.print("Enter Year ");
-// //         int a=s.nextInt();
+//     public static void main(String[] args) {
+//         Scanner s =new Scanner(System.in);
+//         System.out.println("Enter temperature");
+//         int a=s.nextInt();
 
-// //         if (a%4==0) {
-// //             System.out.println("Leap year ");
-// //         }
-// //         else{
-// //             System.out.println("Not a leap year ");
-// //         }
-// //     }
-// // }
+//         if (a>=30) {
+//             System.out.println("Hot Day");
+//         }
+//         else{
+//             System.out.println("normal temperature");
+//         }
+//     }
+// }
 
-// ////Write a program to check whether a number is even or odd.
- 
-// // public class p4 {
 
-// //     public static void main(String[] args) {
-// //         Scanner s =new Scanner(System.in);
-// //         System.out.print("Enter Number  ");
-// //         int a=s.nextInt();
+// Write a program to check whether a given year is leap year or not.
 
-// //         if (a%2==0) {
-// //             System.out.println("Even Number  ");
-// //         }
-// //         else{
-// //             System.out.println("Odd Number  ");
-// //         }
-// //     }
-// // }
+// public class p4 {
 
-// ////Write a program to check whether a number is positive or negative.
+//     public static void main(String[] args) {
+//         Scanner s =new Scanner(System.in);
+//         System.out.print("Enter Year ");
+//         int a=s.nextInt();
+
+//         if (a%4==0) {
+//             System.out.println("Leap year ");
+//         }
+//         else{
+//             System.out.println("Not a leap year ");
+//         }
+//     }
+// }
+
+////Write a program to check whether a number is even or odd.
  
 // public class p4 {
 
@@ -106,11 +88,29 @@
 //         System.out.print("Enter Number  ");
 //         int a=s.nextInt();
 
-//         if (a>=0) {
-//             System.out.println("Positive number ");
+//         if (a%2==0) {
+//             System.out.println("Even Number  ");
 //         }
 //         else{
-//             System.out.println("negetive Number   ");
+//             System.out.println("Odd Number  ");
 //         }
 //     }
 // }
+
+////Write a program to check whether a number is positive or negative.
+ 
+public class p4 {
+
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.print("Enter Number  ");
+        int a=s.nextInt();
+
+        if (a>=0) {
+            System.out.println("Positive number ");
+        }
+        else{
+            System.out.println("negetive Number   ");
+        }
+    }
+}

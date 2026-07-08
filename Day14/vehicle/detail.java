0bb150c13af79b96ad2bdc3354f4a959dc406cb0
Patil Rameshwar <rameshwarr.p001@gmail.com>
2026-07-09 00:54:11 +0530

    8/jul

--- Day14/vehicle/detail.java
+++ Day14/vehicle/detail.java
@@ -1,5 +1,11 @@
 package Day14.vehicle;
 
 public class detail {
-    
+    String vehicleName;
+    String brand;
+    String model;
+    String vehicleNumber;
+    String color;
+    double price;
+    String fuelType;
 }
--- Day14/vehicle/p1.java
+++ Day14/vehicle/p1.java
@@ -1,5 +1,67 @@
 package Day14.vehicle;
 
 public class p1 {
-    
+
+    public static void main(String[] args) {
+
+        detail Vehicle1 = new detail();
+
+        System.out.println("------------Vehicle1---------------");
+        System.out.println(Vehicle1.vehicleName = "Nexon");
+        System.out.println(Vehicle1.brand = "Tata");
+        System.out.println(Vehicle1.model = "2024");
+        System.out.println(Vehicle1.vehicleNumber = "MH14AB1234");
+        System.out.println(Vehicle1.color = "White");
+        System.out.println(Vehicle1.price = 1200000);
+        System.out.println(Vehicle1.fuelType = "Petrol");
+
+        
+        detail Vehicle2 = new detail();
+        
+        System.out.println("------------Vehicle2---------------");
+        System.out.println(Vehicle2.vehicleName = "Creta");
+        System.out.println(Vehicle2.brand = "Hyundai");
+        System.out.println(Vehicle2.model = "2023");
+        System.out.println(Vehicle2.vehicleNumber = "MH12CD5678");
+        System.out.println(Vehicle2.color = "Black");
+        System.out.println(Vehicle2.price = 1800000);
+        System.out.println(Vehicle2.fuelType = "Diesel");
+
+        System.out.println("------------Vehicle3---------------");
+
+        detail Vehicle3 = new detail();
+
+        System.out.println(Vehicle3.vehicleName = "Swift");
+        System.out.println(Vehicle3.brand = "Maruti");
+        System.out.println(Vehicle3.model = "2022");
+        System.out.println(Vehicle3.vehicleNumber = "MH20EF9012");
+        System.out.println(Vehicle3.color = "Red");
+        System.out.println(Vehicle3.price = 850000);
+        System.out.println(Vehicle3.fuelType = "Petrol");
+
+        System.out.println("------------Vehicle4---------------");
+
+        detail Vehicle4 = new detail();
+
+        System.out.println(Vehicle4.vehicleName = "XUV700");
+        System.out.println(Vehicle4.brand = "Mahindra");
+        System.out.println(Vehicle4.model = "2025");
+        System.out.println(Vehicle4.vehicleNumber = "MH01GH3456");
+        System.out.println(Vehicle4.color = "Blue");
+        System.out.println(Vehicle4.price = 2200000);
+        System.out.println(Vehicle4.fuelType = "Diesel");
+
+        System.out.println("------------Vehicle5---------------");
+
+        detail Vehicle5 = new detail();
+
+        System.out.println(Vehicle5.vehicleName = "Activa");
+        System.out.println(Vehicle5.brand = "Honda");
+        System.out.println(Vehicle5.model = "2024");
+        System.out.println(Vehicle5.vehicleNumber = "MH16JK7890");
+        System.out.println(Vehicle5.color = "Grey");
+        System.out.println(Vehicle5.price = 95000);
+        System.out.println(Vehicle5.fuelType = "Petrol");
+    }
+
 }
